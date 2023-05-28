72d73b0436f4b84ff41b51ce37d7a457f4f61a9b
王颖 <wangying@xkool.org>
2023-05-28 16:42:25 +0800

    增加QcloudMediaStorage和QcloudStaticStorage

--- django_storage_qcloud/storage.py
+++ django_storage_qcloud/storage.py
@@ -82,3 +82,82 @@ class QcloudStorage(Storage):
 
         client = CosS3Client(self.config)
         client.delete_object(self.bucket, name)
+
+
+class PrefixedQcloudStorage(QcloudStorage):
+
+    def __init__(self, option=None, prefix=""):
+        super().__init__(option=option)
+        self.prefix = prefix
+
+    def _alter_name(self, name):
+        if self._check_url(name):
+            return name
+        return f"{self.prefix}/{name}"
+
+    def size(self, name):
+        name = self._alter_name(name)
+        return super().size(name)
+
+    def delete(self, name):
+        name = self._alter_name(name)
+        return super().delete(name)
+
+    def url(self, name):
+        name = self._alter_name(name)
+        return super().url(name)
+
+    def exists(self, name):
+        name = self._alter_name(name)
+        return super().exists(name)
+
+    def _open(self, name, mode='rb'):
+        name = self._alter_name(name)
+        return super()._save(name, mode)
+
+    def _save(self, name, content):
+        name = self._alter_name(name)
+        return super()._save(name, content)
+
+    @classmethod
+    def get_relative_location(cls, location):
+        """get relative location for OSS storage. since Django requires settings.MEDIA_ROOT and settings.STATIC_ROOT
+        to be absolute paths, we provide this method to calculate the essential relative path.
+        Most of the case there is a BASE_DIR or ROOT_DIR setting as the root directory of the project, we calculate
+        relative path based on these two settings if they exist."""
+        base_dir = None
+        location = str(location)
+        if hasattr(settings, "BASE_DIR"):           #
+            base_dir = str(settings.BASE_DIR)
+        elif hasattr(settings, "ROOT_DIR"):
+            base_dir = str(settings.ROOT_DIR)
+        if base_dir is None:
+            return location
+        if not location.startswith(base_dir):
+            return location
+        return location[len(base_dir):]
+
+    @classmethod
+    def strip_splash(cls, location):
+        if location.startswith('/'):
+            return location[1:]
+        return location
+
+
+class QcloudMediaStorage(PrefixedQcloudStorage):
+
+    def __init__(self, option=None):
+        prefix = super().strip_splash(
+            super().get_relative_location(settings.MEDIA_ROOT)
+        )
+        super().__init__(option=option, prefix=prefix)
+
+
+class QcloudStaticStorage(PrefixedQcloudStorage):
+
+    def __init__(self, option=None):
+        prefix = super().strip_splash(
+            super().get_relative_location(settings.STATIC_ROOT)
+        )
+        super().__init__(option=option, prefix=prefix)
+
